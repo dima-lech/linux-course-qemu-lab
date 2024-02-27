bd5d709984172ad5da69acdecaf0a29c6052f6d2
dima-lech <dima.lech@gmail.com>
2024-02-27 05:08:29 +0200

    updated pack.sh

--- pack.sh
+++ pack.sh
@@ -8,7 +8,7 @@ fi
 # cp -v bash-5.2.21/bash busybox-1.36.1/_install/bin
 pushd busybox-1.36.1/_install 
 mkdir -p etc
-cp ../../inittab etc
+cp -v ../../inittab etc/
 find . -print0 | cpio --null --create --format=newc --owner=root:root | gzip -9 > ../../$1/initrd.cpio.gz
 popd
 cp -v linux-6.7.5/arch/arm/boot/zImage $1
